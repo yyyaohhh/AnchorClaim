4eb0bb4a888751f3d57e4e6d94e630cb2fa669ed
yyyaohhh <yyyaohhh@gmail.com>
2026-08-20 21:37:25 +0800

    Fix Vercel API bug.

--- api/index.py
+++ api/index.py
@@ -4,5 +4,6 @@ import sys
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if ROOT_DIR not in sys.path:
     sys.path.insert(0, ROOT_DIR)
+sys.path.insert(0, os.path.join(ROOT_DIR, "agent"))
 
 from backend.server import app
